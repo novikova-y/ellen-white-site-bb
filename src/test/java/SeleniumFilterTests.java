8e1ff7e1f3f5db10a015804f14cc53aefa7d8443
novikova-julia <julia.noviova92@gmail.com>
2016-11-02 13:16:09 +0600

    added document filtering test

--- src/test/java/SeleniumFilterTests.java
+++ src/test/java/SeleniumFilterTests.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
 
 import static org.junit.Assert.*;
@@ -31,6 +32,29 @@ public class SeleniumFilterTests {
 
        driver.close();
        driver.quit();
+   }
+
+   @Test
+    public void SeleniumFilterDocsTest () throws InterruptedException {
+
+       System.setProperty("webdriver.chrome.driver", "/home/me/Downloads/chromedriver");
+       WebDriver driver = new ChromeDriver();
+       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+       driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+
+       driver.get("http://ellenwhite.org/library?f%5B0%5D=bundle%3Afiles");
+       WebElement element = driver.findElement(By.id("edit-checkboxes-4-document"));
+       element.click();
+
+       driver.findElement(By.id("edit-filter-button--9")).click();
+
+       Thread.sleep(10000);
+       assertEquals("http://ellenwhite.org/library?f[0]=bundle%3Afiles&f[1]=sm_field_files_primary_media%3Adocument", driver.getCurrentUrl());
+
+
+       driver.close();
+       driver.quit();
+
    }
 
 
