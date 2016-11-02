b5d1c21b5e04b76dcf159c08ce35274f78f2c93f
novikova-julia <julia.noviova92@gmail.com>
2016-11-02 14:03:51 +0600

    added video filtering test

--- src/test/java/SeleniumFilterTests.java
+++ src/test/java/SeleniumFilterTests.java
@@ -12,74 +12,95 @@ import java.util.concurrent.TimeUnit;
 
 public class SeleniumFilterTests {
 
-   @Test
-   public void filterImagesTest () throws InterruptedException {
+    @Test
+    public void filterDocsTest() throws InterruptedException {
 
-       System.setProperty("webdriver.gecko.driver", "/home/me/Downloads/geckodriver");
-       WebDriver driver = new FirefoxDriver();
-       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-       driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        System.setProperty("webdriver.chrome.driver", "/home/me/Downloads/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 
-       driver.get("http://ellenwhite.org/library?f%5B0%5D=bundle%3Afiles");
+        driver.get("http://ellenwhite.org/library?f%5B0%5D=bundle%3Afiles");
+        WebElement element = driver.findElement(By.id("edit-checkboxes-4-document"));
+        element.click();
 
-       WebElement element = driver.findElement(By.id("edit-checkboxes-4-image"));
-       element.click();
+        driver.findElement(By.id("edit-filter-button--9")).click();
 
-       driver.findElement(By.id("edit-filter-button--9")).click();
-
-       Thread.sleep(10000);
-       assertEquals("http://ellenwhite.org/library?f[0]=bundle%3Afiles&f[1]=sm_field_files_primary_media%3Aimage", driver.getCurrentUrl());
-
-       driver.close();
-       driver.quit();
-   }
-
-   @Test
-    public void filterDocsTest () throws InterruptedException {
-
-       System.setProperty("webdriver.chrome.driver", "/home/me/Downloads/chromedriver");
-       WebDriver driver = new ChromeDriver();
-       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-       driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-
-       driver.get("http://ellenwhite.org/library?f%5B0%5D=bundle%3Afiles");
-       WebElement element = driver.findElement(By.id("edit-checkboxes-4-document"));
-       element.click();
-
-       driver.findElement(By.id("edit-filter-button--9")).click();
-
-       Thread.sleep(10000);
-       assertEquals("http://ellenwhite.org/library?f[0]=bundle%3Afiles&f[1]=sm_field_files_primary_media%3Adocument", driver.getCurrentUrl());
+        Thread.sleep(10000);
+        assertEquals("http://ellenwhite.org/library?f[0]=bundle%3Afiles&f[1]=sm_field_files_primary_media%3Adocument", driver.getCurrentUrl());
 
 
-       driver.close();
-       driver.quit();
+        driver.close();
+        driver.quit();
 
-   }
+    }
 
-   @Test
-    public void filterAudiosTest () throws InterruptedException {
+    @Test
+    public void filterImagesTest() throws InterruptedException {
 
-       System.setProperty("webdriver.gecko.driver", "/home/me/Downloads/geckodriver");
-       WebDriver driver = new FirefoxDriver();
-       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-       driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        System.setProperty("webdriver.gecko.driver", "/home/me/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 
-       driver.get("http://ellenwhite.org/library?f%5B0%5D=bundle%3Afiles");
+        driver.get("http://ellenwhite.org/library?f%5B0%5D=bundle%3Afiles");
 
-       WebElement element = driver.findElement(By.id("edit-checkboxes-4-audio"));
-       element.click();
+        WebElement element = driver.findElement(By.id("edit-checkboxes-4-image"));
+        element.click();
 
-       driver.findElement(By.id("edit-filter-button--9")).click();
+        driver.findElement(By.id("edit-filter-button--9")).click();
 
-       Thread.sleep(10000);
-       assertEquals("http://ellenwhite.org/library?f[0]=bundle%3Afiles&f[1]=sm_field_files_primary_media%3Aaudio", driver.getCurrentUrl());
+        Thread.sleep(10000);
+        assertEquals("http://ellenwhite.org/library?f[0]=bundle%3Afiles&f[1]=sm_field_files_primary_media%3Aimage", driver.getCurrentUrl());
 
-       driver.close();
-       driver.quit();
+        driver.close();
+        driver.quit();
+    }
 
+    @Test
+    public void filterAudiosTest() throws InterruptedException {
 
+        System.setProperty("webdriver.gecko.driver", "/home/me/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 
-   }
+        driver.get("http://ellenwhite.org/library?f%5B0%5D=bundle%3Afiles");
+
+        WebElement element = driver.findElement(By.id("edit-checkboxes-4-audio"));
+        element.click();
+
+        driver.findElement(By.id("edit-filter-button--9")).click();
+
+        Thread.sleep(10000);
+        assertEquals("http://ellenwhite.org/library?f[0]=bundle%3Afiles&f[1]=sm_field_files_primary_media%3Aaudio", driver.getCurrentUrl());
+
+        driver.close();
+        driver.quit();
+
+    }
+
+    @Test
+    public void filterVideosTest() throws InterruptedException {
+
+        System.setProperty("webdriver.gecko.driver", "/home/me/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+
+        driver.get("http://ellenwhite.org/library?f%5B0%5D=bundle%3Afiles");
+
+        WebElement element = driver.findElement(By.id("edit-checkboxes-4-video"));
+        element.click();
+
+        driver.findElement(By.id("edit-filter-button--9")).click();
+
+        Thread.sleep(10000);
+        assertEquals("http://ellenwhite.org/library?f[0]=bundle%3Afiles&f[1]=sm_field_files_primary_media%3Avideo", driver.getCurrentUrl());
+
+        driver.close();
+        driver.quit();
+
+    }
 
 }
